952befd8dbe2d5932ef2bcdb8e9d3e617dec195b
Daniel-Astriddao <daniel@astriddao.xyz>
2022-06-25 11:50:50 -0400

    Issue with dependency

--- contracts/StreamRedirect.sol
+++ contracts/StreamRedirect.sol
@@ -1,6 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >= 0.8.0;
 
+import {ISuperfluid, ISuperToken, ISuperApp, ISuperAgreement, SuperAppDefinitions} from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol"; //"@superfluid-finance/ethereum-monorepo/packages/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol";
+import {CFAv1Library} from "@superfluid-finance/ethereum-contracts/contracts/apps/CFAv1Library.sol";
+import {IConstantFlowAgreementV1} from "@superfluid-finance/ethereum-contracts/contracts/interfaces/agreements/IConstantFlowAgreementV1.sol";
+import {SuperAppBase} from "@superfluid-finance/ethereum-contracts/contracts/apps/SuperAppBase.sol";
 
-
-contract StreamRedirect is superApp {
+contract StreamRedirect is SuperAppBase {
   
 }
